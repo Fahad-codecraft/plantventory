44c972318f27f075e8f7e202179e3227705ba3fd
Fahad Devnikar <devnikarfahad@gmail.com>
2025-07-01 09:53:08 +0530

    implemented authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/Hero";
 
 export default function Home() {
     return (
-        <Button>Click me</Button>
+        <Hero />
     );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,19 @@
-import Link from "next/link"
-import { Button } from "./ui/button"
-import { Home, Sprout } from "lucide-react"
-import ModeToggle from "./ModeToggle"
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Home, LogIn, LogOut, Sprout } from "lucide-react";
+import ModeToggle from "./ModeToggle";
+import { stackServerApp } from "@/stack";
+import { getUserDetails } from "@/actions/user.actions";
+import { UserButton } from "@stackframe/stack";
 
-function Navbar() {
+async function Navbar() {
+  const user = await stackServerApp.getUser();
+  const app = stackServerApp.urls;
+  const userProfile = await getUserDetails(user?.id);
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
-
       <div className="max-w-7xl mx-auto px-4">
-
         <div className="flex items-center h-16 justify-between">
           <div className="flex items-center">
             <Link
@@ -19,30 +24,71 @@ function Navbar() {
             </Link>
           </div>
 
+          {/* NavBar Components */}
+
           <div className="hidden md:flex items-center space-x-4">
-            <Button variant={"ghost"} className="flex items-center gap-2" asChild>
+            <Button
+              variant={"ghost"}
+              className="flex items-center gap-2"
+              asChild
+            >
               <Link href="/plants">
                 <Sprout className="w-4 h-4" />
-                <span className="hidden lg:inline">
-                  Plants
-                </span>
+                <span className="hidden lg:inline">Plants</span>
               </Link>
             </Button>
 
-            <Button variant={"ghost"} className="flex items-center gap-2" asChild>
+            <Button
+              variant={"ghost"}
+              className="flex items-center gap-2"
+              asChild
+            >
               <Link href="/">
                 <Home className="w-4 h-4" />
-                <span className="hidden lg:inline">
-                  Home
-                </span>
+                <span className="hidden lg:inline">Home</span>
               </Link>
             </Button>
             <ModeToggle />
+
+            {user ? (
+              <>
+                {/* Sign Out */}
+                <Button
+                  variant={"ghost"}
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signOut}>
+                    <LogOut className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign Out</span>
+                  </Link>
+                </Button>
+
+                <UserButton />
+              </>
+            ) : (
+              <>
+                {/* SignIn */}
+                <Button
+                  variant={"ghost"}
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signIn}>
+                    <LogIn className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign In</span>
+                  </Link>
+                </Button>
+              </>
+            )}
+
+
+
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
