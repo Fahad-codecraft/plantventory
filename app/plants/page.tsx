7ac92799084c892d05b67f37011ab38b04b5fb96
Fahad Devnikar <devnikarfahad@gmail.com>
2025-07-01 10:35:16 +0530

    Implemented table, signin, signup

---
+++ app/plants/page.tsx
@@ -0,0 +1,27 @@
+import InventoryTable from '@/components/InventoryTable';
+import { stackServerApp } from '@/stack';
+import { SignIn, SignUp } from '@stackframe/stack';
+import React from 'react'
+
+async function page() {
+  const user = await stackServerApp.getUser();
+    const app = stackServerApp.urls;
+  return (
+    <>
+    {user ? (
+      <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
+        <InventoryTable />
+      </div>
+    ) : (
+
+      <div className="flex justify-center mt-20 items-center">
+
+        <SignUp />
+      </div>
+
+    )}
+    </>
+  )
+}
+
+export default page
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,38 +1,40 @@
-import { ArrowRight } from "lucide-react";
-
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { ArrowUpRight, CirclePlay } from "lucide-react";
+import React from "react";
 
 const Hero = () => {
   return (
-    <section>
-      <div className="container">
-        <div className="grid items-center gap-8 bg-muted-2 lg:grid-cols-2">
-          <div className="flex flex-col items-center p-16 text-center lg:items-start lg:text-left">
-            <p>New Release</p>
-            <h1 className="my-6 text-4xl font-bold text-pretty lg:text-6xl">
-              Welcome to Our Website
-            </h1>
-            <p className="mb-8 max-w-xl text-muted-foreground lg:text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
-              doloremque mollitia fugiat omnis! Porro facilis quo animi
-              consequatur. Explicabo.
-            </p>
-            <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-              <Button>
-                Primary
-                <ArrowRight className="size-4" />
-              </Button>
-              <Button variant="outline">Secondary</Button>
-            </div>
+    <div className="min-h-screen flex items-center justify-center overflow-hidden">
+      <div className="max-w-screen-xl w-full mx-auto grid lg:grid-cols-2 gap-12 px-6 py-12 lg:py-0">
+        <div className="my-auto">
+          <Badge className="bg-gradient-to-br via-70% from-primary via-muted/30 to-primary rounded-full py-1 border-none">
+            Just released v1.0.0
+          </Badge>
+          <h1 className="mt-6 max-w-[17ch] text-4xl md:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold !leading-[1.2] tracking-tight">
+            Customized Shadcn UI Blocks & Components
+          </h1>
+          <p className="mt-6 max-w-[60ch] text-lg">
+            Explore a collection of Shadcn UI blocks and components, ready to
+            preview and copy. Streamline your development workflow with
+            easy-to-implement examples.
+          </p>
+          <div className="mt-12 flex items-center gap-4">
+            <Button size="lg" className="rounded-full text-base">
+              Get Started <ArrowUpRight className="!h-5 !w-5" />
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              className="rounded-full text-base shadow-none"
+            >
+              <CirclePlay className="!h-5 !w-5" /> Watch Demo
+            </Button>
           </div>
-          <img
-            src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg"
-            alt="placeholder hero"
-            className="h-full w-full object-cover"
-          />
         </div>
+        <div className="w-full aspect-video lg:aspect-auto lg:w-[1000px] lg:h-screen bg-accent rounded-xl lg:rounded-none" />
       </div>
-    </section>
+    </div>
   );
 };
 
