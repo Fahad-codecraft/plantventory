ed5708121926f254234db1cd3c74dc3393d1e75a
Fahad Devnikar <devnikarfahad@gmail.com>
2025-07-02 13:32:01 +0530

    updated

--- app/plants/[slug]/page.tsx
+++ app/plants/[slug]/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import PlantCard from './PlantCard'
+// app/plants/[slug]/page.tsx
+import React from 'react';
+import PlantCard from './PlantCard';
 import { getPlantById } from '@/actions/plant.actions';
 import { stackServerApp } from '@/stack';
 import { SignIn } from '@stackframe/stack';
@@ -16,28 +17,28 @@ export async function generateMetadata(
   };
 }
 
-async function page(context: { params: { slug: string } }) {
-  const { params } = context;
+interface PageProps {
+  params: { slug: string };
+}
+
+export default async function Page({ params }: PageProps) {
   const user = await stackServerApp.getUser();
   const [id] = params.slug.split('--');
   const plant = await getPlantById(id);
 
   if (!user) {
-    return <SignIn />
+    return <SignIn />;
   }
 
-  // if (!plant) {
-  //   return <div className="text-center mt-10">Plant not found.</div>
-  // }
+  if (!plant) {
+    return <div className="text-center mt-10">Plant not found.</div>;
+  }
 
   return (
     <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
-              <div className='lg:col-span-full'>
-    
-                <PlantCard plant={plant} />
-              </div>
-            </div>
-  )
+      <div className="lg:col-span-full">
+        <PlantCard plant={plant} />
+      </div>
+    </div>
+  );
 }
-
-export default page
